683803818ca68b85fc93d1c9fb3e1fb7f7740af2
Sophie kalagbor <sophiek9h@gmail.com>
2025-08-31 00:47:30 +0100

    updated home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,13 +1,60 @@
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import { useGlobalContext } from "@/lib/global-provider";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, SafeAreaView, Text, Touchable, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+
+  const { user } = useGlobalContext();
+
   return (
-    <View
-      className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
 
-      <Text className="font-rubik-light text-xl mb-4">Welcome to the app!</Text>
+        {/* welcome row */}
+        <View className="flex flex-row justify-between items-center mt-12">
+          <View className="flex flex-row items-center gap-3">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-12 rounded-full"
+              resizeMode="contain"
+            />
+            <View>
+              <Text className="text-lg font-rubik-light text-black-100">Good morning</Text>
+              <Text className="text-xl font-rubik-bold text-black-300">{user?.name}</Text>
+            </View>
+          </View>
 
-    </View>
+          <TouchableOpacity>
+            <Image
+              source={icons.bell}
+              className="size-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </View>
+        
+        {/* search component */}
+        <Search/>
+
+        <View className="flex flex-row justify-between items-center my-5">
+          <Text className="font-rubik-semibold text-xl text-black-300">
+            Featured
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base text-primary-300 font-rubik-semibold">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        
+
+      </View>
+
+      
+
+    </SafeAreaView>
   );
 }
